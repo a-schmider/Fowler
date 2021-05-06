b11c00e36d5a6b791ca3c5dbceb58d12acbe834c
ANSR <andreas.schmider98@gmail.com>
2021-05-06 16:39:50 +0200

    Tests added for all models

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -42,5 +42,12 @@ public class Customer {
         return result;
     }
 
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public int getTotalFrequentPoints() {
+        return totalFrequentPoints;
+    }
 }
     
